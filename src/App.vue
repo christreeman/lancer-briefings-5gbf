dcad07546403a64ce2e33f83627bab087f64c2db
christreeman <christreeman@gmail.com>
2023-07-22 16:11:08 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -131,7 +131,7 @@ export default {
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "IPS-N",
           "frame": "Zheng",
-          "mech": "Judau"
+          "mech": "Roger Smith"
         },
         {
           "callsign": "Mozin",
